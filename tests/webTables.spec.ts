a26c633b2b5a4cda8d0acabbe12eaae43f54192b
Ivan Petkov <92800687+ipetkowww@users.noreply.github.com>
2025-07-30 17:40:17 +0300

    Tests for tables
    Test Case 1: Validate the pet name city of the owner
    Test Case 2: Validate owners count of the Madison city
    Test Case 3: Validate search by Last Name
    Test Case 4: Validate phone number and pet name on the Owner Information page
    Test Case 5: Validate pets of the Madison city
    Test Case 6: Validate specialty update
    Test Case 7: Validate specialty lists

--- tests/webTables.spec.ts
+++ tests/webTables.spec.ts
@@ -78,8 +78,6 @@ test.describe("Tests related to OWNERS page", () => {
     })
 })
 
-
-
 test("TC 6: Validate specialty update", async ({ page }) => {
     // 1. Select the VETERINARIANS menu item in the navigation bar, then select "All"
     await page.getByText(/Veterinarians/).click();
@@ -95,20 +93,73 @@ test("TC 6: Validate specialty update", async ({ page }) => {
     // 5. Click on "Edit" button for the "surgery" specialty
     await page.getByRole("row", { name: "surgery" }).getByRole("button", { name: "Edit" }).click();
     // 6. Add assertion "Edit Specialty" page is displayed
+    await page.waitForResponse(/petclinic\/api\/specialties/);
     await expect(page.getByRole("heading", { name: "Edit Specialty" })).toBeVisible();
     // 7. Update the specialty from "surgery" to "dermatology" and click "Update button"
-    await page.locator("#name").clear();
-    await page.locator("#name").fill("dermatology");
-    await page.getByRole("button", {name: "Update"}).click();
-    await expect(page.getByRole("heading", { name: "Specialties" })).toBeVisible();
+    const specialityNameField: Locator = page.getByRole("textbox");
+    await specialityNameField.clear();
+    await specialityNameField.fill("dermatology");
+    await page.getByRole("button", { name: "Update" }).click();
     // 8. Add assertion that "surgery" was changed to "dermatology" in the list of specialties
-    //TODO: review the test as well and add step
+    await expect(page.getByRole("heading", { name: "Specialties" })).toBeVisible();
+    await expect(page.getByRole("row").nth(2).getByRole("textbox")).toHaveValue("dermatology")
     // 9. Select the VETERINARIANS menu item in the navigation bar, then select "All"
     await page.getByText(/Veterinarians/).click();
     await page.getByRole("link", { name: "All" }).click();
     // 10. On the Veterinarians page, add assertion that "Rafael Ortega" has specialty "dermatology"
     await expect(targetRow.locator("td").nth(1)).toHaveText("dermatology");
     // 11. Navigate to SPECIALTIES page, revert the changes renaming "dermatology" back to "surgery"
+    await page.getByRole("link", { name: "Specialties" }).click();
+    await expect(page.getByRole("heading", { name: "Specialties" })).toBeVisible();
+    await page.getByRole("row", { name: "dermatology" }).getByRole("button", { name: "Edit" }).click();
+    await page.waitForResponse(/petclinic\/api\/specialties/);
+    await expect(page.getByRole("heading", { name: "Edit Specialty" })).toBeVisible();
+    await specialityNameField.clear();
+    await specialityNameField.fill("surgery");
+    await page.getByRole("button", { name: "Update" }).click();
+})
+
+test("TC 7: Validate specialty lists", async ({ page }) => {
+    // 1. Select the SPECIALTIES menu item in the navigation bar
+    await page.getByRole("link", { name: "Specialties" }).click();
+    // 2. On the Specialties page, select "Add" button. Type the new specialty "oncology" and click "Save" button
+    await page.getByRole("button", { name: "Add" }).click();
+    await page.locator("#name").fill("oncology");
+    await page.getByRole("button", { name: "Save" }).click();
+    await page.locator(".overlay").waitFor({ state: "detached" });
+    // 3. Extract all values of specialties and put them into the array.
+    const specialities: string[] = [];
+    for (const speciality of await page.getByRole("row").getByRole("textbox").all()) {
+        specialities.push(await speciality.inputValue());
+    }
+    // 4. Select the VETERINARIANS menu item in the navigation bar, then select "All"
+    await page.getByText(/Veterinarians/).click();
+    await page.getByRole("link", { name: "All" }).click();
+    // 5. On the Veterinarians page, locate the "Sharon Jenkins" in the list and click "Edit" button
+    const targetRow: Locator = page.getByRole("row", { name: "Sharon Jenkins" });
+    await targetRow.getByRole("button", { name: "Edit Vet" }).click();
+    // 6. Click on the Specialties drop-down menu. Extract all values from the drop-down menu to an array
+    const veterinarianSpecialities: string[] = [];
+    await page.locator(".dropdown-display").click();
+    for (const veterinarianSpeciality of await page.locator(".dropdown-content label").allTextContents()) {
+        veterinarianSpecialities.push(veterinarianSpeciality);
+    }
+    // 7. Add the assertion that array of specialties collected in the step 3 is equal the the array from drop-down menu
+    expect(specialities.length).toEqual(veterinarianSpecialities.length);
+    expect(specialities.every((val, index) => val === veterinarianSpecialities[index])).toBeTruthy();
+    // 8. Select the "oncology" specialty and click "Save vet" button
+    await page.getByRole("checkbox", {name: "oncology"}).check();
+    await page.locator(".dropdown-display").click();
+    await page.getByRole("button", {name: "Save Vet"}).click();
+    // 9. On the Veterinarians page, add assertion, that "Sharon Jenkins" has specialty "oncology"
+    await expect(targetRow.locator("td").nth(1)).toHaveText("oncology");
+    // 10. Navigate to SPECIALTIES page. Click "Delete" for "oncology" specialty
+    await page.getByRole("link", { name: "Specialties" }).click();
+    await page.getByRole("row", {name: "oncology"}).getByRole("button", {name: "Delete"}).click();
+    // 11. Navigate to VETERINARIANS page. Add assertion that "Sharon Jenkins" has no specialty assigned
+    await page.getByText(/Veterinarians/).click();
+    await page.getByRole("link", { name: "All" }).click();
+    await expect(targetRow.locator("td").nth(1)).toBeEmpty();
 })
 
 
